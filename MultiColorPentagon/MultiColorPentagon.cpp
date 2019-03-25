2594112727a8b5fb25e38e2d61890d79f4a5c9d2
dionsaputra <dionsaputra666@gmail.com>
2019-03-25 08:46:53 +0700

    Finish multicolor pentagon

--- MultiColorPentagon/MultiColorPentagon.cpp
+++ MultiColorPentagon/MultiColorPentagon.cpp
@@ -28,11 +28,11 @@ int main() {
     glDeleteShader(fragmentShader);
     
     float vertices[] = {
-        -0.6, -0.9, 0,
-        0.6, -0.9, 0,
-        0.97, 0.14, 0,
-        0, 0.85f, 0,
-        -0.97, 0.14, 0
+        -0.6, -0.9, 0, 1, 0, 0,
+        0.6, -0.9, 0, 1, 1, 0,
+        0.97, 0.14, 0, 0.5, 0.5, 0.5,
+        0, 0.85f, 0, 0, 1, 1,
+        -0.97, 0.14, 0, 0, 0, 1
     }; 
 
     unsigned int indices[] = {
@@ -54,16 +54,21 @@ int main() {
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    // position attribute    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0); 
 
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0,0,0,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
