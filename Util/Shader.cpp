f3a05303403c345bb498d26a9f8d0816f732114d
dionsaputra <dionsaputra666@gmail.com>
2019-04-21 21:39:11 +0700

    Add some asset

--- Util/Shader.cpp
+++ Util/Shader.cpp
@@ -94,3 +94,7 @@ void Shader::setVec3(const std::string &name, float x, float y, float z) const {
 void Shader::setFloat(const std::string &name, float value) const { 
     glUniform1f(glGetUniformLocation(id, name.c_str()), value); 
 }
+
+void Shader::setInt(const std::string &name, int value) const { 
+    glUniform1i(glGetUniformLocation(id, name.c_str()), value); 
+}
