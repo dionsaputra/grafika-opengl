d6964b3134bbf45c737dfc14e3ec4d716b806afd
dionsaputra <dionsaputra666@gmail.com>
2019-05-01 22:51:40 +0700

    Minor bug in bind buffer

--- SuzukiJimnyParticle/ParticleGenerator.cpp
+++ SuzukiJimnyParticle/ParticleGenerator.cpp
@@ -74,7 +74,7 @@ public:
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(particle), particle, GL_STREAM_DRAW);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
     }
 
